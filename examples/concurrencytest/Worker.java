e6d071faa8e50120a2e2bef111286acc0121a912
yilianz <yilianz@usca.edu>
2021-10-07 14:55:45 -0400

    update concurrency

--- examples/concurrencytest/Worker.java
+++ examples/concurrencytest/Worker.java
@@ -28,17 +28,17 @@ public class Worker implements Runnable {
 			//nap for a while
 			SleepUtilities.nap();
 			
-		   sem.acquire();  //entry section
+	//	   sem.acquire();  //entry section
 			
 			//critical section
 			System.out.println("WORKER "+ name + " requests clipboard");
 			board.setText("WORKER "+ name+ " is using clipboard");
 //*** problem here 
-			System.out.println("WORKER"+ name+"get"+board.getText());
+			System.out.println("WORKER "+ name+" get "+board.getText());
 			System.out.println("WORKER "+ name + " exits clipboard");
 			
 			
-		    sem.release();   //exit section
+	//	    sem.release();   //exit section
 		
 		}
 	}
